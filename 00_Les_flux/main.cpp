6ceaaa314f754f6aeca719752eccee6883457dc6
valentin peltier <54929298+vpeltier@users.noreply.github.com>
2019-09-10 08:42:31 +0200

    Update main.cpp

--- 00_Les_flux/main.cpp
+++ 00_Les_flux/main.cpp
@@ -1,33 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*
+ * File:   main.cpp
+ * Author: vpeltier
+ *
+ * Created on 5 septembre 2019
+ */
 #include <iostream>
-#include <iomanip>
 #include <fstream>
 #include <string>
+#include <iomanip>
+
 using namespace std;
+
 int main() {
-    string nomDuFichier;
+    string inputFile;
+
     cout << "Entrer le nom du fichier à lire : ";
-    cin >> nomDuFichier;
-    //Création du flux en lecture sur le fichier
-    ifstream leFichier(nomDuFichier.c_str()); // c_str() transforme string en char*
-    if (!leFichier .is_open())
+    cin >> inputFile;
+
+    ifstream fichier(inputFile.c_str()); //Creation du flux en lecture sur le fichier
+    if (!fichier.is_open())
         cerr << "Erreur lors de l'ouverture du fichier" << endl;
+
     else
     {
         string pays;
         int nbOr;
         int nbArgent;
         int nbBronze;
-        cout << "+" << setfill('-') << setw(21) << "+" << setw(6) << "+" << setw(6) << "+" << setw(6) << "+" << setfill(' ') << endl;
-        do
+
+        //affichage de la premiere ligne du tableau
+        cout << setfill('-');
+        cout << "+" << setw(18) << "+" << setw(9) << "+" << setw(9) << "+" << setw(9) << "+" << endl;
+        cout << setfill(' ');
+
+        while (!fichier.eof())
         {
-            //récupération des valeurs
-            leFichier >> pays >> nbOr >> nbArgent >> nbBronze;
-            if (leFichier.good())//Si les valeurs ont bien été lues
+            fichier >> pays >> nbOr >> nbArgent >> nbBronze; //recuperation des valeurs
+            if (fichier.good())//Si les valeurs ont bien été lues
             {
-                cout << "|" << setw(20) << left << pays << "|" << setw(5) << right << nbOr << "|" << setw(5) << right << nbArgent << "|" << setw(5) << right << nbBronze << "|" << endl;
+                //affichage des lignes du tableau
+                cout << "| " << left << setw(16) << pays << "|" << right << setw(7) << nbOr << " |" << right << setw(7) << nbArgent << " |" << right << setw(7) << nbBronze << " |" << endl;
             }
-        } while (!leFichier.eof());
-        cout << "+" << setfill('-') << setw(21) << "+" << setw(6) << "+" << setw(6) << "+" << setw(6) << "+" << setfill(' ') << endl;
+        }
+        //Affichage du bas du tableau.
+        cout << setfill('-');
+        cout << "+" << setw(18) << "+" << setw(9) << "+" << setw(9) << "+" << setw(9) << "+" << endl;
     }
     return 0;
 }
